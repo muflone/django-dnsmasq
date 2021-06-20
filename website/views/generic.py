7d1c6f251217e1860b37c988ee83afcc600c6788
Muflone <webreg@muflone.com>
2021-06-21 00:47:21 +0200

    Added GenericMixin for any page view

---
+++ website/views/generic.py
@@ -0,0 +1,40 @@
+##
+#     Project: Django dnsmasq
+# Description: A Django application to create dnsmasq configuration
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2021 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+from django.views.generic import View
+from django.views.generic.base import ContextMixin
+
+from project import PRODUCT_NAME, VERSION
+
+
+class GenericMixin(ContextMixin,
+                   View):
+    """Generic mixin"""
+    extra_context = {
+        'app_name': PRODUCT_NAME,
+        'app_version': VERSION,
+    }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(ContextMixin.get_context_data(self, **kwargs))
+        context['request_path'] = self.request.path
+        context['page_title'] = self.page_title
+        return context

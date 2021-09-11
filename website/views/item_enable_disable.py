8232de4807f1746997b9a4ca8d1662d0e66057af
Muflone <webreg@muflone.com>
2021-09-12 01:50:19 +0200

    New views ItemEnableDisableView, ItemEnableView and ItemDisableView to set the is_active status for an object

---
+++ website/views/item_enable_disable.py
@@ -0,0 +1,32 @@
+##
+#     Project: Django dnsmasq
+# Description: A Django application to create dnsmasq configuration
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2021 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+from django.views.generic import RedirectView
+
+
+class ItemEnableDisableView(RedirectView):
+    """
+    Change is_active status for an object
+    """
+    def get_redirect_url(self, *args, **kwargs):
+        if item := self.model.objects.filter(pk=self.kwargs['pk']).first():
+            item.is_active = self.is_active
+            item.save()
+        return super().get_redirect_url(*args, **kwargs)
